771e9d8c5c96213d1ee32525f39309f2360969b3
Simon Duff <simon.j.duff@gmail.com>
2018-07-10 11:46:38 +0100

    Dice now respect new spec

--- src/components/diceTray.js
+++ src/components/diceTray.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class DiceTray extends React.Component{
+    constructor(props){
+        super(props);
+        this.rollDice = this.rollDice.bind(this);
+        this.dice = [];
+    }
+
+    rollDice(){
+        this.dice.forEach(d => {
+            d.roll();
+        });
+    }
 
     render(){
         var specs = this.props.specs || [];
         var dice = [];
         specs.forEach(spec => {
-            console.log(`Processing spec ${spec.count}d${spec.size}`);
-            for (var i =0;i<spec.size;i++){
+            for (var i =0;i<spec.count;i++){
                 dice.push(spec.size);
             }
         });
@@ -17,19 +27,30 @@ class DiceTray extends React.Component{
                 Total {dice.length}
                 <div>
                     {(dice).map((size, index) => {
-                        return <Dice key={index} size={size} />
+                        return <Dice key={`${index}-${size}`} size={size} ref={d => {this.dice.push(d)}} />
                     })}
                 </div>
+                <button onClick={this.rollDice}>Roll</button>
             </div>
         );
     }
 }
 
 class Dice extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value:this.props.size};
+    }
+
+    roll(){
+        const min = 1;
+        this.setState({value: Math.floor(Math.random() * (this.props.size - min +1)) + min});
+    }
+
     render(){
         return (
-            <span class="dice">
-                d{this.props.size}
+            <span className="dice">
+                {this.state.value}
             </span>
         );
     }
